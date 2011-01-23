e1d8e2021f17856916fce90846e7ff600d0393d0
Pieter Michels <pieter.michels@gmail.com>
2011-01-23 21:02:11 +0100

    edits

--- makro_scraper.js
+++ makro_scraper.js
@@ -1,21 +1,16 @@
-var request = require('request'),
-		http = require('http'),
-		$ = require('jquery');
+var request = require('request'), http = require('http'), $ = require('jquery');
 
 http.createServer(function (req, res) {
 	request({uri: 'http://www.makro.be/Content/assortiment/benzinestation/benzineprijzen/1/index.jsp?stat='}, function (error, response, body) {
 		res.writeHead(200, {'Content-Type': 'text/xml'});
 
 		var fuel_types = ['diesel', 'eurosuper', 'superplus'];
-		var i = 0;
 
 		res.write('<?xml version="1.0" encoding="UTF-8"?>\n');
 		res.write('<fuels>\n');
 		
-		$(body).find('div.boxContent table[width="337"]').each(function() { 
-			res.write('<' + fuel_types[i] + '>' + $(this).find('tr:first-child').next().find('td:last').html() + '</' + fuel_types[i] + '>\n'); 
-
-			i++;
+		$(body).find('div.boxContent table[width="337"]').each(function(index, element) { 
+			res.write('<' + fuel_types[index] + '>' + element.find('tr:first-child').next().find('td:last').html() + '</' + fuel_types[index] + '>\n'); 
 		});
 		
 		res.end('</fuels>\n');
