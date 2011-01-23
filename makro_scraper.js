4a9bb332ce0f764f40f9fe32f31e339d45450f7b
Pieter Michels <pieter.michels@gmail.com>
2011-01-23 20:17:51 +0100

    edits

--- makro_scraper.js
+++ makro_scraper.js
@@ -3,9 +3,6 @@ var request = require('request'),
 		sys = require('sys'), 
 		http = require('http'),
 		$ = require('jquery');
-		
-var url = 'http://www.makro.be/Content/assortiment/benzinestation/benzineprijzen/1/index.jsp?stat=';
-var fuel_types = ['diesel', 'eurosuper', 'superplus'];
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/xml'});
@@ -13,20 +10,19 @@ http.createServer(function (req, res) {
   res.write('<?xml version="1.0" encoding="UTF-8"?>\n');
 	res.write('<fuels>\n');
 	
+	var fuel_types = ['diesel', 'eurosuper', 'superplus'];
 	var fuels = [];
 	var i = 0;
 	
-  request({uri: url}, function (error, response, body) {
-	console.log('ingeladen');
+  request({uri: 'http://www.makro.be/Content/assortiment/benzinestation/benzineprijzen/1/index.jsp?stat='}, function (error, response, body) {
 		$(body).find('div.boxContent table[width="337"]').each(function() { 
-			console.log(fuels);
 			fuels.push('<' + fuel_types[i] + '>' + $(this).find('tr:first-child').next().find('td:last').html() + '</' + fuel_types[i] + '>\n'); 
 			
 			i++;
 		});
 	});
 	
-	console.log(fuels);
+	console.log(fuels[0]);
 	
 	for(var j = 0; j < fuels.length; j++)
 		res.write(fuels[j]);
