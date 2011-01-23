1a2f19cd49c7425dc78f6f2268f3d1e2d808be38
Pieter Michels <pieter.michels@gmail.com>
2011-01-23 20:59:43 +0100

    edits

--- makro_scraper.js
+++ makro_scraper.js
@@ -8,21 +8,20 @@ var i = 0;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/xml'});
-
-  res.write('<?xml version="1.0" encoding="UTF-8"?>\n');
-	res.write('<fuels>\n');
 	
-
 	i = 0;
 	
 	request({uri: 'http://www.makro.be/Content/assortiment/benzinestation/benzineprijzen/1/index.jsp?stat='}, function (error, response, body) {
+		res.write('<?xml version="1.0" encoding="UTF-8"?>\n');
+		res.write('<fuels>\n');
+		
 		$(body).find('div.boxContent table[width="337"]').each(function() { 
 			http_response = http_response + '<' + fuel_types[i] + '>' + $(this).find('tr:first-child').next().find('td:last').html() + '</' + fuel_types[i] + '>\n'; 
 
 			i++;
 		});
+		
+		res.write(http_response);
+		res.end('</fuels>\n');
 	});
-	
-	res.write(http_response);
-	res.end('</fuels>\n');
 }).listen(8124);
