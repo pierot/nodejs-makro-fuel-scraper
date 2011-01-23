bdc2da638e6eea63394878ec3213d3ec2af3d6bd
Pieter Michels <pieter.michels@gmail.com>
2011-01-23 21:00:46 +0100

    edits

--- makro_scraper.js
+++ makro_scraper.js
@@ -1,27 +1,23 @@
 var request = require('request'),
 		http = require('http'),
 		$ = require('jquery');
-		
-var http_response = '-';
-var fuel_types = ['diesel', 'eurosuper', 'superplus'];
-var i = 0;
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-	
-	i = 0;
-	
 	request({uri: 'http://www.makro.be/Content/assortiment/benzinestation/benzineprijzen/1/index.jsp?stat='}, function (error, response, body) {
+		res.writeHead(200, {'Content-Type': 'text/xml'});
+
+		var fuel_types = ['diesel', 'eurosuper', 'superplus'];
+		var i = 0;
+
 		res.write('<?xml version="1.0" encoding="UTF-8"?>\n');
 		res.write('<fuels>\n');
 		
 		$(body).find('div.boxContent table[width="337"]').each(function() { 
-			http_response = http_response + '<' + fuel_types[i] + '>' + $(this).find('tr:first-child').next().find('td:last').html() + '</' + fuel_types[i] + '>\n'; 
+			res.write('<' + fuel_types[i] + '>' + $(this).find('tr:first-child').next().find('td:last').html() + '</' + fuel_types[i] + '>\n'); 
 
 			i++;
 		});
 		
-		res.write(http_response);
 		res.end('</fuels>\n');
 	});
 }).listen(8124);
